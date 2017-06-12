0e29ded6279ec573b629b1efdfe241a317d635da
BelenMayo <belenmaydi@hotmail.com>
2017-06-11 21:44:25 -0300

    Listado pizzas con API + cambios en estilos
    Listado pizzas con API + cambios en estilos

--- TPLab4/src/app/app.component.ts
+++ TPLab4/src/app/app.component.ts
@@ -14,9 +14,7 @@ const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 })
 export class AppComponent {
 
-constructor(){}
-
-// PUNTUAR
+//////////////////////////////////////////////// PUNTUAR
   public max:number = 10;
   public rate:number = 7;
   public isReadonly:boolean = false;
@@ -33,5 +31,43 @@ constructor(){}
     this.overStar = void 0;
   }
 
-//
+/////////////////////////////////////////////// ABM PIZZA
+
+  title: string= 'Listado de Pizzas';
+  public id= "";
+  public nombre= "";
+  public precio= "";
+  public foto= "";
+
+  public datosPizzas: Array<any>;
+
+  constructor(public datosPizz: PizzeriaService){
+    console.log(this.datosPizzas);
+    datosPizz.traerTodasLasPizzas()
+    .then(datosPizz => {
+      console.info("datos pizza", datosPizz);
+      this.datosPizzas= datosPizz;
+    })
+  }
+
+  traerUnaPizza($id){
+    this.datosPizz.traerUnaPizza($id)
+      .then(datosPizz => {
+      console.info("datos persona", datosPizz);
+      this.datosPizzas= datosPizz;
+    })
+  }
+
+  borrarPizza($id){
+    this.datosPizz.eliminarPizza($id);
+  }
+  
+  altaPizza(){
+    let nuevaPersona={nombre:this.id,
+                      apellido:this.nombre,
+                      foto:this.precio,
+                      password:this.foto
+                    };
+    this.datosPizz.agregarPizza(nuevaPersona);
+  }
 }
